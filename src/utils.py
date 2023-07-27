d1e769c20256bc1734554c9bfc923bb3cd119f5d
ojaffe <oliver.jaffe@hotmail.co.uk>
2023-07-27 20:23:07 +0100

    improved logging

--- src/utils.py
+++ src/utils.py
@@ -18,5 +18,5 @@ def print_trainable_parameters(model):
         if param.requires_grad:
             trainable_params += param.numel()
     print(
-        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable: {100 * trainable_params / all_param:.2f}%"
     )
